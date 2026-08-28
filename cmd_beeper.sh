cfbdafdf86beda4129e33c453901eda4613979f3
Diego Fernández Menéndez <diegofm499@gmail.com>
2026-08-28 02:27:37 +0200

    Add custom sound and timeout

--- cmd_beeper.sh
+++ cmd_beeper.sh
@@ -16,10 +16,38 @@ cleanup () {
     exit
 }
 
-trap 'cleanup' SIGINT SIGTERM SIGALRM
+while getopts ":s:t:h" opt; do
+    case $opt in
+        s) sound=$OPTARG ;;
+        t) 
+            timer=$OPTARG
+            if [[ ! $timer =~ ^[0-9]+$ ]]; then
+                echo "${0}: timeout must be a positive integer"
+                exit 1
+            fi
+            ;;
+        h)
+            echo -e "usage: ${0} [-s sound_file] [-t timeout]\n\n See the man page for more information"
+            exit 0
+            ;;
+        \?) 
+            echo "${0}: unknown option -$OPTARG"
+            exit 1
+            ;;
+        :) echo "${0}: -$OPTARG requires an argument"
+            exit 1
+            ;;
+    esac
+done
 
-loop_sound $sound &
+trap 'cleanup' SIGINT SIGTERM
 
-read -r -s -p $'Press Enter to stop...\n'
+loop_sound "$sound" &
+
+if [[ -v timer ]]; then
+    read -r -s -p -t "${timer?}" $'Press Enter to stop...\n'
+else
+    read -r -s -p $'Press Enter to stop...\n'
+fi
 cleanup
 
